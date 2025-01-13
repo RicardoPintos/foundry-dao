2e4898e29642677135fe76be01437e2f040361c9
Ricardo Pintos <pintosric@gmail.com>
2025-01-13 20:04:09 -0300

    Fix Solidity version

--- script/DeployDao.s.sol
+++ script/DeployDao.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import {MyGovernor} from "../src/MyGovernor.sol";
 import {GovToken} from "../src/GovToken.sol";
